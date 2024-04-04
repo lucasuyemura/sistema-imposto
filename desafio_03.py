537e15a05d260c8195077091231afd015de98434
Lucas Uyemura <153959422+lucasuyemura@users.noreply.github.com>
2024-04-04 11:25:41 -0300

    Delete desafio_03.py

--- desafio_03.py
+++
@@ -1,6 +0,0 @@
-def somaImposto(custo, imposto):
-    custo_com_imposto = custo * imposto
-    print(f"O custo era de {custo}, com imposto passará a ser de {custo_com_imposto:.2f}!")
-
-somaImposto(100, 0.3)
-
