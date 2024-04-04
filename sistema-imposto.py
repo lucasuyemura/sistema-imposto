27962afc5164dba78968a97851bf80b3dc43c652
lucasuyemura <lucasuyemura@gmail.com>
2024-04-04 14:22:52 +0000

    Alteração nome do arquivo

---
+++ sistema-imposto.py
@@ -0,0 +1,6 @@
+def somaImposto(custo, imposto):
+    custo_com_imposto = custo * imposto
+    print(f"O custo era de {custo}, com imposto passará a ser de {custo_com_imposto:.2f}!")
+
+somaImposto(100, 0.3)
+
